0dc807cb77eef543936398fc8db29d1c36f2979b
mqfrommee <mqfromme@mail.ru>
2024-12-27 19:09:53 +0300

    реальное плаг дербмо

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from waitress import serve
+import logging
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    logging.info("Запуск приложения..")
+
+    with app.app_context():
+        db.create_all()  
+        logging.info("База данных инициализирована.")
+
+    logging.info("Запуск сервера Waitress на порту 8000...")
+    serve(app, host='127.0.0.1', port=8000)
